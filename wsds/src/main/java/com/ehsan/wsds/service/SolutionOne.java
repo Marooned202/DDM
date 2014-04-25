686b56e2b2596e66e2b9c02ccceddab5790e090c
unknown <ehkasl@gmail.com>
2014-04-25 01:22:21 -0400

    changes

--- wsds/src/main/java/com/ehsan/wsds/service/SolutionOne.java
+++ wsds/src/main/java/com/ehsan/wsds/service/SolutionOne.java
@@ -10,8 +10,8 @@ import java.util.Set;
 import com.ehsan.wsds.util.CommunityUtils;
 
 public class SolutionOne {
-	
-	public static final int MAX_TIME = 63;
+
+	public static final int MAX_TIME = 10;
 
 	public int[][] findAllBestArray(List<List<Integer>> templateVector, String filename) { 
 		int[][] best = new int[templateVector.size()][MAX_TIME];
@@ -83,80 +83,138 @@ public class SolutionOne {
 		return best;
 	}
 
-	public double[][][] extractMatrix (List<Set<Integer>> templateVector, String filename) { 
-		double[][][] matrix = new double[templateVector.size()][templateVector.size()][MAX_TIME];
+	public double[][] extractMatrix (List<Set<Integer>> templateVector, int[][] mark, String filename, int time) { 
+		double[][] matrix = new double[templateVector.size()][templateVector.size()];
 
 		for (int t = 0; t < MAX_TIME; t++) {
 			for (int i = 0; i < matrix[0].length; i++) {
 				for (int j = 0; i < matrix[0].length; i++) {
-					matrix[i][j][t] = -1;
+					matrix[i][j] = -1;
 				}
 			}
 		}
 
-		for (int time = 0; time < MAX_TIME; time++) {
-			try {			
-				BufferedReader br = new BufferedReader(new FileReader(filename + time));
-				String line;			
-				int row = 0;
-				int num = 0;
-				while ((line = br.readLine()) != null) {
-					if (line.trim().isEmpty()) continue;				
-					String[] split = line.split("\\s+");
-					for (int i = 0;i < split.length; i++) {
-						if (split[i].equals("X")) { 
-							matrix[row][i][time] = -1;
-							continue;
-						}
-						matrix[row][i][time] = Double.parseDouble(split[i]);
+		//for (int time = 0; time < MAX_TIME; time++) {
+		try {			
+			BufferedReader br = new BufferedReader(new FileReader(filename + time));
+			String line;			
+			int row = 0;
+			int num = 0;
+			while ((line = br.readLine()) != null) {
+				if (line.trim().isEmpty()) continue;				
+				String[] split = line.split("\\s+");
+				for (int i = 0;i < split.length; i++) {
+					if (split[i].equals("X")) { 
+						matrix[row][i] = -1;
+						continue;
 					}
-					row++;
+					if (mark[row][i] == -1) {
+						matrix[row][i] = -1;
+						continue;
+					}
+					matrix[row][i] = Double.parseDouble(split[i]);
 				}
-			} catch (Exception e) {
-				e.printStackTrace();
+				row++;
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+		//}
 
 		return matrix;
 	}
-	
+
 	public void markMatrix (double[][][] matrix, int i, int j, double value) {		
 		for (int time = 0; time < MAX_TIME; time++) {
 			matrix[i][j][time] = value;
 		}
 	}
 
+	public void pickCommunities(List<Set<Integer>> templateVector, List<Set<Integer>> services, double[][] matrix, int[][] markMatrix, int time) {	
+		List<Set<Integer>> copyForLoopServices = new ArrayList<Set<Integer>>();
+		copyForLoopServices.addAll(services);
+		for (Set<Integer> serviceGroup: copyForLoopServices) {
+			Set<Integer> best = findBestCommunity (templateVector, services, serviceGroup, matrix, markMatrix, time);
+			if (serviceGroup.equals(findBestCommunity (templateVector, services, best, matrix, markMatrix, time))) {
+				System.out.println("ServiceGroup: " + serviceGroup + " and " + best + " are double bests.");		
+
+				Set<Integer> newGroup = new HashSet<Integer>();
+				newGroup.addAll(serviceGroup);
+				newGroup.addAll(best);				
+
+				if (!services.contains(newGroup)) {
+					System.out.println("New group formed: " + newGroup);
+					services.add(newGroup);
+				}
+
+				int i = templateVector.indexOf(serviceGroup);
+				int j = templateVector.indexOf(best);
+				int n = templateVector.indexOf(newGroup);
+				markMatrix[i][j] = -1;
+				//				matrix[i][j] = -1;
+				markMatrix[j][i] = -1;
+				//				matrix[j][i] = -1;
+				markMatrix[i][n] = -1;
+				markMatrix[n][i] = -1;
+				markMatrix[j][n] = -1;
+				markMatrix[n][j] = -1;
+			}
+		}		
+	}
+
+	public Set<Integer> findBestCommunity(List<Set<Integer>> templateVector,
+			List<Set<Integer>> services, Set<Integer> serviceGroup,	double[][] matrix, int[][] markMatrix, int time) {
+
+		Set<Integer> result = null;
+
+		double value = -0.5;
+		for (Set<Integer> otherServiceGroup: services) {
+			if (matrix[templateVector.indexOf(serviceGroup)][templateVector.indexOf(otherServiceGroup)] > value) {
+				value = matrix[templateVector.indexOf(serviceGroup)][templateVector.indexOf(otherServiceGroup)];
+				result = otherServiceGroup;
+			}
+		}		
+
+		return result;		
+	}
+
 	public void run (List<Set<Integer>> templateVector, String filename) {
 
 		//int[][] bestArray = findAllBestArray(templateVector, filename);
 
-		List<Set<Integer>> initialSingleServices = CommunityUtils.extractSingleServices(templateVector);
-		List<Set<Integer>> test = new ArrayList<Set<Integer>>();
 
+		List<Set<Integer>> test = new ArrayList<Set<Integer>>();
 		HashSet<Integer> newCommunity = new HashSet<Integer>();
 		newCommunity.add(2571);
 		test.add(newCommunity);
-
 		newCommunity = new HashSet<Integer>();
 		newCommunity.add(3035);		
 		test.add(newCommunity);
-
 		newCommunity = new HashSet<Integer>();
 		newCommunity.add(1731);		
 		test.add(newCommunity);
-
 		newCommunity = new HashSet<Integer>(); 
 		newCommunity.add(1024);
 		newCommunity.add(938);
 		newCommunity.add(2074);
 		newCommunity.add(1789);
-		test.add(newCommunity);
+		test.add(newCommunity);	
+		newCommunity = new HashSet<Integer>();
+		newCommunity.add(1731);		
+		test.add(newCommunity);		
+		System.out.println(test.indexOf(newCommunity));
 
 		//int[] bestTest = findVectorBestArrayTime(templateVector, initialSingleServices, filename, 0);
-		double[][][] matrix = extractMatrix(templateVector, filename);
-		
-		
 
-		System.out.println(matrix);
+		int[][] markMatrix = new int[templateVector.size()][templateVector.size()];
+		List<Set<Integer>> services = CommunityUtils.extractSingleServices(templateVector);
+
+		for (int time = 0; time < 5; time++) {			
+			System.out.println("\nTime: " + time);
+			double[][] matrix = extractMatrix(templateVector, markMatrix, filename, time);
+			pickCommunities (templateVector, services, matrix, markMatrix, time);
+		}
+
 	}
+
 }
